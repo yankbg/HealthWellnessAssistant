43879b889801b94f77617b6c1a8f055ebd222cc3
YANNICK kabunga <yankbg2@gmail.com>
2025-10-01 20:42:19 +0300

    setup the project

---
+++ app/src/main/java/com/example/heathwellnessassistant/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.heathwellnessassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
