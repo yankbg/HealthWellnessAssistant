5296a7fb6d5dfd5f84fb427a88d5809c4d835461
YANNICK kabunga <yankbg2@gmail.com>
2025-10-24 19:38:55 +0300

    add delete and update method in the Dao

--- app/src/main/java/com/example/heathwellnessassistant/Database/JournalEntryDao.java
+++ app/src/main/java/com/example/heathwellnessassistant/Database/JournalEntryDao.java
@@ -3,8 +3,11 @@ package com.example.heathwellnessassistant.Database;
 
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import com.example.heathwellnessassistant.Entities.JournalEntry;
 
@@ -14,14 +17,18 @@ import java.util.List;
 @Dao
 public interface JournalEntryDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     public void insert(JournalEntry journalEntry);
 
-
+    @Update
+    public void update(JournalEntry journalEntry);
 
     @Query("SELECT * FROM JournalEntry")
     public List<JournalEntry> getJournalEntry();
 
     @Query("SELECT * FROM JournalEntry WHERE journal_id = :id")
     public JournalEntry getResult(long id);
+
+    @Delete
+    public void deleteJournalEntry(JournalEntry journalEntry);
 }
