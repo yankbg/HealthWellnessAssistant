71ec0c87f46e9f135e804801b95d65fe21e75021
YANNICK kabunga <yankbg2@gmail.com>
2025-11-01 11:17:22 +0300

    since theJunit is failing without any reason, I tried to implement the MainActivity with a small use case for testing

--- app/src/main/java/com/example/heathwellnessassistant/MainActivity.java
+++ app/src/main/java/com/example/heathwellnessassistant/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.heathwellnessassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.heathwellnessassistant.Entities.JournalEntry;
+import com.example.heathwellnessassistant.ViewModel.JournalViewModel;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    EditText inputField;
+    TextView outputFiled;
+    Button addBtn;
+    JournalViewModel journalViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputField = findViewById(R.id.etText_context);
+        outputFiled = findViewById(R.id.tv01);
+        addBtn = findViewById(R.id.button);
+
+        journalViewModel = new ViewModelProvider(this).get(JournalViewModel.class);
+
+        addBtn.setOnClickListener(v -> {
+            String entryText = inputField.getText().toString();
+            if(!entryText.isEmpty()){
+                JournalEntry newEntry = new JournalEntry();
+                newEntry.setText_context(entryText);
+                newEntry.setDate(new Date());
+                journalViewModel.insetJournalEntry(newEntry);
+                inputField.setText("");
+            }
+        });
+
+        journalViewModel.getJournalEntryAll().observe(this, entries ->{
+            StringBuilder allEntries = new StringBuilder();
+            for(JournalEntry entry: entries){
+                allEntries.append(entry.getText_context()).append("");
+            }
+            outputFiled.setText(allEntries.toString());
+        });
     }
 }
