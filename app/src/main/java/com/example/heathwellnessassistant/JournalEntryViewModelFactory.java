a2b4c97c5e5e9bf94963991cfc6ff83e3c126891
YANNICK kabunga <yankbg2@gmail.com>
2026-04-10 18:30:54 +0300

    JournalEntryViewModelFactory for avoiding miss linking it will allow to create an object into our UI for viewmodel

--- app/src/main/java/com/example/heathwellnessassistant/JournalEntryViewModelFactory.java
+++ app/src/main/java/com/example/heathwellnessassistant/JournalEntryViewModelFactory.java
@@ -1,6 +1,25 @@
 package com.example.heathwellnessassistant;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.heathwellnessassistant.Repository.JournalEntryRepository;
+import com.example.heathwellnessassistant.ViewModel.JournalViewModel;
+
 public class JournalEntryViewModelFactory implements ViewModelProvider.Factory{
+    private final JournalEntryRepository repository;
+
+    public JournalEntryViewModelFactory(JournalEntryRepository repository) {
+        this.repository = repository;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass == JournalViewModel.class) {
+            return (T) new JournalViewModel(repository);
+        }
+        throw new IllegalArgumentException("Unknown ViewModel class: " + modelClass);
+    }
 }
