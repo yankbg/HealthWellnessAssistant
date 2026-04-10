3ca7ed56d6efdf8f07ac744567fc62fb9448feba
YANNICK kabunga <yankbg2@gmail.com>
2026-04-10 14:10:14 +0300

    we create analyzeText method for taking juornal text and return label pus the confidence

--- app/src/main/java/com/example/heathwellnessassistant/DataLayer/TfLiteSentimentAnalyzer.java
+++ app/src/main/java/com/example/heathwellnessassistant/DataLayer/TfLiteSentimentAnalyzer.java
@@ -1,16 +1,19 @@
 package com.example.heathwellnessassistant.DataLayer;
 
 import android.content.Context;
+import android.util.Log;
 
 
+import org.tensorflow.lite.support.label.Category;
 import org.tensorflow.lite.task.text.nlclassifier.NLClassifier;
 
-
+import java.util.List;
 
 
 public class TfLiteSentimentAnalyzer {
     private static final String MODEL_FILE = "emotion_model.tflite";
     private final NLClassifier classifier;
+    private static final String TAG = "TfLiteSentimentAnalyzer";
 
 
     public TfLiteSentimentAnalyzer(Context context) {
@@ -26,6 +29,36 @@ public class TfLiteSentimentAnalyzer {
             );
         }catch (Exception e){
             throw new IllegalStateException("Failed to load sentiment model", e);
+        }
+    }
+    public String analyzeText(String text){
+        if (text == null || text.trim().isEmpty()){
+            return "Empty input text";
+        }
+        try{
+            // 1. Run inference: get all categories (emotions)
+            List<Category> results = classifier.classify(text);
+
+            if(results == null || results.isEmpty()){
+                return "Unknown";
+            }
+            // 2. Take the top‑scoring class (ArgMax)
+            Category top = results.get(0);
+            String label = top.getLabel();
+            float confidence = top.getScore();
+
+            // 3. Format: "joy with 85.0% confidence"
+            return String.format(
+                    "%s with %.1f%% confidence",
+                    label,
+                    confidence * 100
+            );
+
+        }catch (Exception e){
+            e.printStackTrace();
+            Log.d(TAG, "analyzeText: "+e);
+
+            return "error";
         }
     }
 }
